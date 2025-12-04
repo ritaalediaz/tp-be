81f92b3c4d8ba530f1a05e1a6e32af231a350dd0
RitaDiaz <ritaalediaz@gmail.com>
2025-12-04 14:24:04 -0300

    cors

--- Desktop/tp-be/src/main.ts
+++ Desktop/tp-be/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
       "http://localhost:5173"               // 👈 para desarrollo local
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true, // ponelo en true si usás cookies o auth
+    credentials: true,
   });
 
   app.useGlobalPipes(
