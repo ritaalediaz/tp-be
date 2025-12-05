baa21b634000734610f071b3552ed36fcea457cb
RitaDiaz <ritaalediaz@gmail.com>
2025-12-05 15:07:13 -0300

    pizza

--- Desktop/tp-be/src/personalizar-pizzas/dto/create-pizza-personalizada.dto.ts
+++ Desktop/tp-be/src/personalizar-pizzas/dto/create-pizza-personalizada.dto.ts
@@ -1,7 +1,19 @@
+import { IsString, IsNumber, IsArray, ArrayMinSize } from 'class-validator';
+
 export class CreatePizzaPersonalizadaDto {
+  @IsString()
   nombre: string;
+
+  @IsString()
   masa: string;
+
+  @IsString()
   salsa: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
   ingredientes: string[];
+
+  @IsNumber()
   precio: number;
 }
